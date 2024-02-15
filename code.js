d9fa69a685b1ffbe427df5e55a90fb7da5615e5d
Bella Moss <152352216+bellamoss77@users.noreply.github.com>
2024-02-14 23:59:57 -0500

    add image files and create array of image objects

---
+++ code.js
@@ -0,0 +1,144 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let slides = [{
+        src: 'images/Ap1.png',
+        alt: 'Dogs',
+        title: 'Apollo 1',
+    }, {
+        src: 'images/At1.png',
+        alt: 'Dogs',
+        title: 'Athena 1',
+    }, {
+        src: 'images/Ap2.png',
+        alt: 'Dogs',
+        title: 'Apollo 2',
+    }, {
+        src: 'images/At2.png',
+        alt: 'Dogs',
+        title: 'Athena 2',
+    }, {
+        src: 'images/Ap3.png',
+        alt: 'Dogs',
+        title: 'Apollo 3',
+    }, {
+        src: 'images/At3.png',
+        alt: 'Dogs',
+        title: 'Athena 3',
+    }, {
+        src: 'images/Ap4.png',
+        alt: 'Dogs',
+        title: 'Apollo 4',
+    }, {
+        src: 'images/At4.png',
+        alt: 'Dogs',
+        title: 'Athena 4',
+    }, {
+        src: 'images/Ap5.png',
+        alt: 'Dogs',
+        title: 'Apollo 5',
+    }, {
+        src: 'images/At5.png',
+        alt: 'Dogs',
+        title: 'Athena 5',
+    }, {
+        src: 'images/Ap6.png',
+        alt: 'Dogs',
+        title: 'Apollo 6',
+    }, {
+        src: 'images/At6.png',
+        alt: 'Dogs',
+        title: 'Athena 6',
+    }, {
+        src: 'images/Ap7.png',
+        alt: 'Dogs',
+        title: 'Apollo 7',
+    }, {
+        src: 'images/At7.png',
+        alt: 'Dogs',
+        title: 'Athena 7',
+    }, {
+        src: 'images/Ap8.png',
+        alt: 'Dogs',
+        title: 'Apollo 8',
+    }, {
+        src: 'images/At8.png',
+        alt: 'Dogs',
+        title: 'Athena 8',
+    }, {
+        src: 'images/Ap9.png',
+        alt: 'Dogs',
+        title: 'Apollo 9',
+    }, {
+        src: 'images/At9.png',
+        alt: 'Dogs',
+        title: 'Athena 9',
+    }, {
+        src: 'images/Ap10.png',
+        alt: 'Dogs',
+        title: 'Apollo 10',
+    }, {
+        src: 'images/At10.png',
+        alt: 'Dogs',
+        title: 'Athena 10',
+    }, {
+        src: 'images/Ap11.png',
+        alt: 'Dogs',
+        title: 'Apollo 11',
+    }, {
+        src: 'images/At11.png',
+        alt: 'Dogs',
+        title: 'Athena 11',
+    }, {
+        src: 'images/Ap12.png',
+        alt: 'Dogs',
+        title: 'Apollo 12',
+    }, {
+        src: 'images/At12.png',
+        alt: 'Dogs',
+        title: 'Athena 12',
+    }, {
+        src: 'images/Ap13.png',
+        alt: 'Dogs',
+        title: 'Apollo 13',
+    }, {
+        src: 'images/At13.png',
+        alt: 'Dogs',
+        title: 'Athena 13',
+    }, {
+        src: 'images/Ap14.png',
+        alt: 'Dogs',
+        title: 'Apollo 14',
+    }, {
+        src: 'images/At14.png',
+        alt: 'Dogs',
+        title: 'Athena 14',
+    }, {
+        src: 'images/Ap15.png',
+        alt: 'Dogs',
+        title: 'Apollo 15',
+    }, {
+        src: 'images/At1.png',
+        alt: 'Dogs',
+        title: 'Athena 15',
+    }, {
+        src: 'images/ApAt15.png',
+        alt: 'Dogs',
+        title: 'Apollo & Athena 1',
+    }, {
+        src: 'images/ApAt2.png',
+        alt: 'Dogs',
+        title: 'Apollo & Athena 2',
+    }, {
+        src: 'images/ApAt3.png',
+        alt: 'Dogs',
+        title: 'Apollo & Athena 3',
+    }, {
+        src: 'images/ApAt4.png',
+        alt: 'Dogs',
+        title: 'Apollo & Athena 4',
+    }, {
+        src: 'images/ApAt5.png',
+        alt: 'Dogs',
+        title: 'Apollo & Athena 5',
+    }
+    ]
+})
